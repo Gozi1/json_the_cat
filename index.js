b89d283424c5042abf977ae86cf55e72d24e96cd
Gozi1 <snwabiani@gmail.com>
2022-11-01 19:51:31 +0000

    should have commited earlier but made changes to let me test the code added and index file that has a callback to decide its it has an error refactored breedfetcher to a modular form

---
+++ index.js
@@ -0,0 +1,12 @@
+const myArgs = process.argv.slice(2);
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
