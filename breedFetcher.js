1b9998f8306acee6fb566b955ec00bb83dbfef06
Gozi1 <snwabiani@gmail.com>
2022-11-01 19:59:02 +0000

    remove comments from breed_fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,6 @@
 // takes an agruments as a cat breed and returns some description about that breed 
 const request = require('request');
-// const breedFetcher = (breed) =>{
-// request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-//   if(error){
-//     console.log(error);
-//     return
-//   }
-//   const data = JSON.parse(body);
-//   if(data.length === 0){
-//     console.log(`${breed} does not exist`);
-//     return 
-//   }
-//   console.log(data[0].description);
-//   console.log(typeof data);
-  
-// });
-// }
+
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   if(error){
